f5c13589b46de79de4e1ecc205b51800ef29f286
leonardo benavides <leorbenavidesm@gmail.com>
2022-08-12 14:40:19 -0400

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loading:{
+      titulo:"",
+      estado:false,
+      color: 'primarys'
+    }
   },
   getters: {
   },
   mutations: {
+    mostrarLoading(state, payload){
+      state.loading.titulo= payload.titulo
+      state.loading.color= payload.color
+      state.loading.estado= true
+    },
+    ocultarLoading(state){
+      state.loading.estado= false;
+    }
   },
   actions: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <v-row>
+    <v-col md="12" >
+      <v-card>
+        <v-date-picker 
+        v-model="picker"
+        full-width
+        locale="es"
+        :min="minimo"
+        :max="maximo"
+        @change=" getDolar(picker)"
+        >
+        
+        </v-date-picker>
+      </v-card>
+      <v-card color="error" dark>
+        <v-card-text class="display-1 text-md-center text-sm-center" >
+          {{valor}}
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script> 
+  import axios from 'axios';
+  import { mapMutations } from 'vuex';
+  export default {
+    data(){
+      return{
+        picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        minimo:'1984',
+        maximo:new Date().toISOString().substr(0, 10),
+        valor: 0
+      }
+    },
+    methods:{
+      ...mapMutations(['mostrarLoading','ocultarLoading']),
+      async getDolar(dia){
+        let arrayFecha= dia.split('-').reverse().join('-')
+        try{
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+          this.mostrarLoading({titulo:'Accediendo a informacion', color:'secondary'})
+          let datos= await axios.get(`https://mindicador.cl/api/dolar/${arrayFecha}`);
+
+          if(datos.data.serie.length > 0){
+            this.valor= await datos.data.serie[0].valor;
+          }else{
+            this.valor= "sin resultados"
+          }
+        }catch(error){
+          console.log(error)
+        }finally{
+          this.ocultarLoading()
+        }
+ 
+      }
+    },
+    created(){
+      this.getDolar(this.picker)
+    }
   }
-}
 </script>
